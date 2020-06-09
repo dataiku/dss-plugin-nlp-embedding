7d699b9a4beb649090240b8815197429e994d707
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-06-09 19:26:33 +0200

    Edited file 'python-runnables/download-pretrained-embedding/runnable.py'

--- python-runnables/download-pretrained-embedding/runnable.py
+++ python-runnables/download-pretrained-embedding/runnable.py
@@ -27,6 +27,8 @@ class MyRunnable(Runnable):
         self.config = config
         self.plugin_config = plugin_config
         self.client = dataiku.api_client()
+        print("heeere")
+        print(get_recipe_resource())
         self.params = json.load(os.path.join(get_recipe_resource(),"models_download_links.json"))
 
     def get_progress_target(self):
