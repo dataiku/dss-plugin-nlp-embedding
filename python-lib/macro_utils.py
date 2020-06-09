19a5e269b4f4e227a0761d60f82cf24faedee142
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-06-09 19:08:12 +0200

    Edited file 'python-lib/macro_utils.py'

--- python-lib/macro_utils.py
+++ python-lib/macro_utils.py
@@ -56,7 +56,7 @@ class fasttext_downloader():
         self.folder = folder
         self.language = language   
         self.file_name = "fasttext_" + str(self.language)
-        print(get_recipe_resource())
+        print("heeeere" + get_recipe_resource())
         self.params = json.load(os.path.join(get_recipe_resource(),"models_download_links.json"))[self.file_name]
 
 
