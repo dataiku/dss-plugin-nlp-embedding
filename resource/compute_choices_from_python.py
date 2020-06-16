c0206c1295c7416e22543d532b81badf9184d465
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-06-16 10:50:04 +0200

    Edited file 'resource/compute_choices_from_python.py'

--- resource/compute_choices_from_python.py
+++ resource/compute_choices_from_python.py
@@ -6,9 +6,17 @@ def do(payload, config, plugin_config, inputs):
     print("config: {}".format(config))
     print("plugin_config: {}".format(plugin_config))
     print("inputs: {}".format(inputs))
-    values = MODEL_CONFIFURATIONS.keys()
-    labels = [x.lower().replace(" ","_") for x in MODEL_CONFIFURATIONS.keys()]
-    choices = [{"value": v, "label": l} for v,l in zip(labels,values)]
+    parameter_name =  payload["parameterName"]
+    if parameter_name == "embedding_model":
+        values = MODEL_CONFIFURATIONS.keys()
+        labels = [x.lower().replace(" ","_") for x in MODEL_CONFIFURATIONS.keys()]
+        choices = [{"value": v, "label": l} for v,l in zip(labels,values)]
+
+    elif parameter_name == "language":
+        model = config["embedding_model"]
+        values = MODEL_CONFIFURATIONS[model]
+    
+    
     print(stop)
     
     return {"choices": choices}
