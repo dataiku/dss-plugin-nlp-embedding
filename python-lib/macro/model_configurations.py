926c7c762cb5b36977f5d90c703f4e01c66c35ba
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-06-16 10:49:43 +0200

    Edited file 'python-lib/macro/model_configurations.py'

--- python-lib/macro/model_configurations.py
+++ python-lib/macro/model_configurations.py
@@ -1,90 +1,122 @@
 MODEL_CONFIFURATIONS = {
-    "Word2vec": {
-        "English": {
-            "model_link" : "https://docs.google.com/uc?export=download",
-            "id_gdrive": "0B7XkCwpI5KDYNlNUTTlSS21pQmM"
-        },
-        "Dutch": {
-            "model_id" : 39 
+    
+    "word2vec": {
+        "label": "Word2vec",
+        "languages": {
+            "English": {
+                "model_link" : "https://docs.google.com/uc?export=download",
+                "id_gdrive": "0B7XkCwpI5KDYNlNUTTlSS21pQmM"
+            },
+            "Dutch": {
+                "model_id" : 39 
+            }
         }
     },
 
-    "FastText": {
-        "English":  "en",
-        "Arabic": "ar"
+    "fasttext": {
+        "label": "FastText",
+        "languages": {
+            "English":  "en",
+            "Arabic": "ar"
+        }        
     },
 
-    "Glove": {
-        "English": {
-            "model_link": "http://nlp.stanford.edu/data/glove.42B.300d.zip"
-        }
-    },
-
-    "ELMo": {
-        "English": {
-            "model_link": "https://tfhub.dev/google/elmo/3?tf-hub-format=compressed"
-        }
-    },
-
-    "Universal Sentence Encoder": {
-        "Multilingual": {
-            "model_link": "https://tfhub.dev/google/universal-sentence-encoder-multilingual/3"
-        },
-        "English": {
-            "model_link": "https://tfhub.dev/google/universal-sentence-encoder/4?tf-hub-format=compressed"
+    "glove": {
+        "label": "Glove",
+        "languages": {
+            "English": {
+                "model_link": "http://nlp.stanford.edu/data/glove.42B.300d.zip"
+            }
         }
         
     },
 
-    "Transformers": {
-        "BERT": {
-            "Multilingual": [
-                "bert-base-multilingual-uncased",
-                "bert-base-multilingual-cased"
-
-            ],
-            "English": [
-                "bert-base-uncased",
-                "bert-large-uncased",
-                "bert-base-cased",
-                "bert-large-cased",
-                "bert-large-uncased-whole-word-masking",
-                "bert-large-cased-whole-word-masking"
-            ],
-
-            "Chinese": [
-                "bert-base-chinese"
-            ],
-
-            "German": [
-                "bert-base-german-cased"
-            ],
-
-            "Japanese": [
-                "cl-tohoku/bert-base-japanese"
-            ]
-        },
-
-        "DistilBERT": {
-            "English": [
-                "distilbert-base-uncased",
-                "distilbert-base-uncased-distilled-squad",
-                "distilbert-base-cased"
-            ],
-
-            "German": [
-                "distilbert-base-german-cased"
-            ]
-        },
-
-        "GPT-2": {
-            "English": [
-                "gpt2",
-                "gpt2-medium",
-                "gpt2-large",
-                "gpt2-xl"
-            ]
+    "elmo": {
+        "label": "ELMo",
+        "languages": {
+            "English": {
+                "model_link": "https://tfhub.dev/google/elmo/3?tf-hub-format=compressed"
+            }
         }
+       
+    },
+
+    "use": {
+        "label": "Universal Sentence Encoder",
+        "languages": {
+            "Multilingual": {
+                "model_link": "https://tfhub.dev/google/universal-sentence-encoder-multilingual/3"
+            },
+            "English": {
+                "model_link": "https://tfhub.dev/google/universal-sentence-encoder/4?tf-hub-format=compressed"
+            }
+        }
+    },
+
+    "tranformers": {
+        "label": "Transformers",
+        "architectures": {
+            "bert": {
+                "label": "BERT",
+                "languages"; {
+                    "Multilingual": [
+                    "bert-base-multilingual-uncased",
+                    "bert-base-multilingual-cased"
+
+                    ],
+                    "English": [
+                        "bert-base-uncased",
+                        "bert-large-uncased",
+                        "bert-base-cased",
+                        "bert-large-cased",
+                        "bert-large-uncased-whole-word-masking",
+                        "bert-large-cased-whole-word-masking"
+                    ],
+
+                    "Chinese": [
+                        "bert-base-chinese"
+                    ],
+
+                    "German": [
+                        "bert-base-german-cased"
+                    ],
+
+                    "Japanese": [
+                        "cl-tohoku/bert-base-japanese"
+                    ]
+                }
+                
+            },
+
+            "distilbert": {
+                "label": "DistilBERT",
+                "languages": {
+                    "English": [
+                        "distilbert-base-uncased",
+                        "distilbert-base-uncased-distilled-squad",
+                        "distilbert-base-cased"
+                    ],
+
+                    "German": [
+                        "distilbert-base-german-cased"
+                    ]
+                }
+                
+            },
+
+            "gpt2": {
+                "label": "GPT-2",
+                "languages": {
+                    "English": [
+                        "gpt2",
+                        "gpt2-medium",
+                        "gpt2-large",
+                        "gpt2-xl"
+                    ]
+                }
+                
+            }
+        }   
     }
 
 
