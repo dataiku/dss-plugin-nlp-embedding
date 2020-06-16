c14545786612b7adf3eff6bd8853cdbeabd16bc9
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-06-16 10:27:22 +0200

    Edited file 'resource/compute_choices_from_python.py'

--- resource/compute_choices_from_python.py
+++ resource/compute_choices_from_python.py
@@ -3,9 +3,9 @@ from macro.model_configurations import MODEL_CONFIFURATIONS
 
 def do(payload, config, plugin_config, inputs):
     print("payload: {}".format(payload))
-    print("payload: {}".format(config))
-    print("payload: {}".format(plugin_config))
-    print("payload: {}".format(inputs))
+    print("config: {}".format(config))
+    print("plugin_config: {}".format(plugin_config))
+    print("inputs: {}".format(inputs))
     values = MODEL_CONFIFURATIONS.keys()
     labels = [x.lower().replace(" ","_") for x in MODEL_CONFIFURATIONS.keys()]
     choices = [{"value": v, "label": l} for v,l in zip(labels,values)]
