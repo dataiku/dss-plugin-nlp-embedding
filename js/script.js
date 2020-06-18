de29f673960590f05f0ab2f83e0d9d76c06d503d
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-06-18 17:26:33 +0200

    Edited file 'js/script.js'

--- js/script.js
+++ js/script.js
@@ -2,12 +2,22 @@ var app = angular.module('modelDownloader.build', []);
 
 app.controller('modelDownloaderController', function($scope) {
 
+    $scope.getModels = function(){
+        $scope.callPythonDo({method: "get_models"}).then(function(data){
+        $scope.models = data['models']
+        }); 
+        
+        $scope.showModelList=true;
+        $scope.showLanguageList=false;
+
+    };
+
     var init = function(){
         $scope.callPythonDo({method: "get_languages"}).then(function(data){
          $scope.languages = data['languages']
          }); 
          $scope.showLanguageList=true;
- 
+         $scope.showModelList=false;
          
      };
          
