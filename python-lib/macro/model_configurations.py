299b8bbf5a7ec60d4d12cdfd8ca7a8dc63126688
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-06-18 12:01:59 +0200

    Edited file 'python-lib/macro/model_configurations.py'

--- python-lib/macro/model_configurations.py
+++ python-lib/macro/model_configurations.py
@@ -5,7 +5,7 @@ MODEL_CONFIFURATIONS = {
         "languages": {
             "english": {
                 "model_link" : "https://docs.google.com/uc?export=download",
-                "id_gdrive": "0B7XkCwpI5KDYeFdmcVltWkhtbmM"
+                "id_gdrive": "0B7XkCwpI5KDYNlNUTTlSS21pQmM"
             },
             "dutch": {
                 "model_id" : 39 
