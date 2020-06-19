dcfcd7f58be26112fe49b9c015c9f3d1bd49f879
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-06-19 16:04:04 +0200

    Edited file 'resource/recipe-helper.py'

--- resource/recipe-helper.py
+++ resource/recipe-helper.py
@@ -1,6 +1,5 @@
 import dataiku
-from macro.model_configurations import (MODELS_CONF,
-                                        TRANSFORMERS_CONF)
+from macro.model_configurations import MODEL_CONFIFURATIONS
 
 def do(payload, config, plugin_config, inputs):
     if payload["method"] == "get_languages":
@@ -9,15 +8,13 @@ def do(payload, config, plugin_config, inputs):
     if payload["method"] == "get_models":
         return get_models(config)
         
-    if payload["method"] == "get_architectures":
-        return get_architectures(config)
+    if payload["method"] == "get_transformer_model_versions":
+        return get_transformer_model_versions(config)
 
 
 def get_languages():
     languages= []
-    conf_list = [MODELS_CONF,TRANSFORMERS_CONF]
-    for conf in conf_list:
-        languages.extend([m["language_list"] for m in conf.values()])
+    languages = [m["language_list"] for m in MODEL_CONFIFURATIONS.values()]
     languages = [item for sublist in languages for item in sublist]
     return {'languages': list(set(languages))}
 
@@ -25,10 +22,10 @@ def get_languages():
 def get_models(config):
     language = config.get("language")
     models = []
-    conf_list = [MODELS_CONF,TRANSFORMERS_CONF]
-    for conf in conf_list:
-        models.extend([m["family"] for m in conf.values() if language in m["language_list"]])
+    models = [m["family"] for m in MODEL_CONFIFURATIONS.values() if language in m["language_list"]]
     return {'models': models}
 
-def get_architectures(config):
+def get_transformer_model_versions(config):
+    model = config.get("modelName")
+    [x["label"] for x in MODEL_CONFIFURATIONS.values() if x["family"] == ]
     return {"architectures": ["foo"]}
