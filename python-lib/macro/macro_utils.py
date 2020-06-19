ee757f82ef3bf9b41143ddbe175791ef9efec4aa
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-06-19 19:16:13 +0200

    Edited file 'python-lib/macro/macro_utils.py'

--- python-lib/macro/macro_utils.py
+++ python-lib/macro/macro_utils.py
@@ -1,7 +1,14 @@
+from macro.model_configurations import MODEL_CONFIFURATIONS
+
 def read_model_inputs(config):
     macro_inputs = {}
     macro_inputs["language"] = config.get("language",None)
-    macro_inputs["embedding_model"] = config.get("modelName",None)
+    #Add mapping language ISO <--> Language readable format
+    
+    model_name = config.get("modelName",None)
+    model_id =[x["id"] for x in MODEL_CONFIFURATIONS.values() if x["label"] == model_name][0] 
+    macro_inputs["embedding_model"] = model_id
+    
     macro_inputs["output_folder_name"] = config.get("outputFolder",None)
     macro_inputs["transformer_shortcut_name"] = config.get("transformersModelVersion",None)
     
