4239dbc0cbbb27f4f1ae6712a5819c643ae9491a
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-06-19 11:11:13 +0200

    Edited file 'python-runnables/download-pretrained-embedding/runnable.py'

--- python-runnables/download-pretrained-embedding/runnable.py
+++ python-runnables/download-pretrained-embedding/runnable.py
@@ -40,6 +40,9 @@ class MyRunnable(Runnable):
     def run(self, progress_callback):
 
         # Retrieving parameters
+        print("heere")
+        print(self.config)
+        print(something)
         macro_inputs = read_model_inputs(self.config)
 
         # Creating new Managed Folder if needed
