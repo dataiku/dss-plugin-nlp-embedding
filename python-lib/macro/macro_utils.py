a5c1529e6bdeefd0f0662b055c7819bb6401152f
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-06-19 19:08:21 +0200

    Edited file 'python-lib/macro/macro_utils.py'

--- python-lib/macro/macro_utils.py
+++ python-lib/macro/macro_utils.py
@@ -1,10 +1,9 @@
 def read_model_inputs(config):
     macro_inputs = {}
-
-    macro_inputs["output_folder_name"] = config.get("outputFolder",None)
     macro_inputs["language"] = config.get("language",None)
     macro_inputs["embedding_model"] = config.get("modelName",None)
-    
+    macro_inputs["output_folder_name"] = config.get("outputFolder",None)
+    macro_inputs["transformer_shortcut_name"] = config.get("transformersModelVersion",None)
     
     proxy = {}
     macro_inputs["proxy"] = proxy
