e967b6dcaa206f58effac2f95d8e57ed4c6db196
api:MpiXIZJxmkWYyCWv <api:MpiXIZJxmkWYyCWv>
2020-07-07 10:31:17 +0200

    Edited file 'python-lib/macro/macro_utils.py'

--- python-lib/macro/macro_utils.py
+++ python-lib/macro/macro_utils.py
@@ -11,16 +11,5 @@ def read_model_inputs(config):
 
     macro_inputs["output_folder_name"] = config.get("outputFolder",None)
     macro_inputs["transformer_shortcut_name"] = config.get("transformersModelVersion",None)
-    
-    proxy = {}
-    macro_inputs["proxy"] = proxy
-    '''
-    advanced_settings = config['advanced_settings']
-    if advanced_settings:
-        proxy = config['proxy_config']
-        if proxy:
-            proxy = proxy["custom_proxy_config"]
-    macro_inputs["proxy"] = proxy
-    '''
 
     return macro_inputs
